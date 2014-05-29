eba542099c5725c7b617cb86d85ded930070acba
Alexandra Beall <bealla@uci.edu>
2014-05-28 20:14:49 -0700

    created buttons

--- blackjack/app/src/main/java/com/grandpad/blackjack/app/MainActivity.java
+++ blackjack/app/src/main/java/com/grandpad/blackjack/app/MainActivity.java
@@ -1,20 +1,18 @@
-package com.grandpad.blackjack.app;
+    package com.grandpad.blackjack.app;
 
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+    import android.os.Bundle;
+    import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+    public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_screen);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.start_screen);
+        }
+        Intent myIntent = new Intent(MainActivity.this, NextActivity.class);
+
+        CurrentActivity.this.startActivity(myIntent);
+
     }
-
-}
