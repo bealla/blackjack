e2a33967829d24879cb03d768663c36015b1dc6c
Daniel yang <endqwerty@gmail.com>
2014-05-29 01:25:15 -0700

    adding changing views

--- blackjack/app/src/main/java/com/grandpad/blackjack/app/MainActivity.java
+++ blackjack/app/src/main/java/com/grandpad/blackjack/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.grandpad.blackjack.app;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    int chips;
 
     //buttons on the right
     Button btn_split;
@@ -34,10 +40,26 @@ public class MainActivity extends ActionBarActivity {
     ImageView img_player_1;
     ImageView img_player_2;
 
+    TextView tv_text_bank;
+    TextView tv_text_bet;
+
+    //card arrays
+    int[] deck = new int[53];
+    int[] dealer_hand = new int[10];
+    int[] player_hand = new int[10];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        chips = settings.getInt("chips", 1000); //get saved chips. if non-existent set to 1000
+
+        for (int i : deck) {
+            deck[i] = i;
+        }
+
         btn_split = (Button) findViewById(R.id.btn_split);
         btn_double = (Button) findViewById(R.id.btn_double);
         btn_hit = (Button) findViewById(R.id.btn_hit);
@@ -57,14 +79,18 @@ public class MainActivity extends ActionBarActivity {
         img_player_1 = (ImageView) findViewById(R.id.img_player_1);
         img_player_2 = (ImageView) findViewById(R.id.img_player_2);
 
-        setBetScreen();
+        tv_text_bank = (TextView) findViewById(R.id.tv_text_bank);
+        tv_text_bet = (TextView) findViewById(R.id.tv_text_bet);
+
+        tv_text_bank.setText(chips);
+
+        //setBetScreen();
         setFirstChoice();
-        setHitScreen();
+        //setHitScreen();
     }
 
 
     public void setBetScreen() {
-        //TODO: fill in
         img_deck.setVisibility(View.INVISIBLE);
         img_dealer_revealed.setVisibility(View.INVISIBLE);
         img_dealer_hidden.setVisibility(View.INVISIBLE);
@@ -73,32 +99,45 @@ public class MainActivity extends ActionBarActivity {
         btn_minus_one.setBackgroundResource(R.drawable.penny);
         btn_plus_one.setBackgroundResource(R.drawable.penny);
         btn_minus_five.setBackgroundResource(R.drawable.nickel);
-        btn_minus_five.setBackgroundResource(R.drawable.nickel);
+        btn_plus_five.setBackgroundResource(R.drawable.nickel);
+        btn_minus_one.setTextColor(Color.TRANSPARENT);
+        btn_plus_one.setTextColor(Color.TRANSPARENT);
+        btn_minus_five.setTextColor(Color.TRANSPARENT);
+        btn_plus_five.setTextColor(Color.TRANSPARENT);
     }
 
     private void setFirstChoice() {
         //TODO: fill in
+        btn_minus_one.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_plus_one.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_minus_five.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_plus_five.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+
+        btn_minus_one.setTextColor(Color.DKGRAY);
+        btn_plus_one.setTextColor(Color.DKGRAY);
+        btn_minus_five.setTextColor(Color.DKGRAY);
+        btn_plus_five.setTextColor(Color.DKGRAY);
 
     }
 
     private void setHitScreen() {
         //TODO: fill in
+        btn_minus_one.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_plus_one.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_minus_five.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+        btn_plus_five.setBackgroundResource(R.drawable.abc_item_background_holo_light);
+
     }
 
     public void hitCommands(View view) {
         // If hit is clicked hide all buttons besides hit and stand
         //TODO: add another card
-        btn_double.setVisibility(View.GONE);
-        btn_split.setVisibility(View.GONE);
 
     }
 
     public void doubledownCommands(View view) {
         // If doubleDown is clicked hide all buttons besides next round (not made yet)
         // TODO: double the bet and add last card
-        btn_double.setVisibility(View.GONE);
-        btn_split.setVisibility(View.GONE);
-        btn_bet.setVisibility(View.GONE);
 
     }
 
