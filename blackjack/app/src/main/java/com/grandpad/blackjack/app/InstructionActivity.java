9af331a691864a647483c0c8dd887a7f05987fa6
Alexandra Beall <bealla@uci.edu>
2014-05-29 22:56:24 -0700

    flashing arrow

--- blackjack/app/src/main/java/com/grandpad/blackjack/app/InstructionActivity.java
+++ blackjack/app/src/main/java/com/grandpad/blackjack/app/InstructionActivity.java
@@ -5,16 +5,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 
 public class InstructionActivity extends FirstActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.howto_screen);
+        ImageView arrow = (ImageView) findViewById(R.id.arrow);
+
+        arrow=(ImageView)findViewById(R.id.arrow);
+        Animation mAnimation = new AlphaAnimation(1, 0);
+        mAnimation.setDuration(300);
+        mAnimation.setInterpolator(new LinearInterpolator());
+        mAnimation.setRepeatCount(Animation.INFINITE);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        arrow.startAnimation(mAnimation);
+        arrow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                v.clearAnimation();
+
+
+            }
+        });
 
     }
-
-
 }
+
+
+
+
+
+
+
+
