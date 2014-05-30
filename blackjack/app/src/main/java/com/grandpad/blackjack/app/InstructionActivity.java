d0cb2da249646f5de8256742ecdf10e7a79acdf8
Alexandra Beall <bealla@uci.edu>
2014-05-29 21:54:44 -0700

    created and linked how to play screen

---
+++ blackjack/app/src/main/java/com/grandpad/blackjack/app/InstructionActivity.java
@@ -0,0 +1,20 @@
+package com.grandpad.blackjack.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+public class InstructionActivity extends FirstActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howto_screen);
+
+    }
+
+
+}
